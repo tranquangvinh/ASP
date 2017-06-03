a9bc8e07c83606cf262aec544d4042355784bc85
tranquangvinh <tranquangvinh5899@gmail.com>
2017-06-03 01:03:34 -0700

    khoi tao project

---
+++ 1461467DAWEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1461467DAWEB.MvcApplication" Language="C#" %>
---
+++ 1461467DAWEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1461467DAWEB.Startup))]
+namespace _1461467DAWEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
