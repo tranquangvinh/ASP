622ddfa003c01db4c80d6e88e2bddf51135fa34d
tranquangvinh <tranquangvinh5899@gmail.com>
2017-06-04 13:09:33 -0700

    them tinymce va hoan thanh chuc nang them loai san pham

---
+++ 1461467DAWEB/Areas/Admin/Controllers/ProductTypeController.cs
@@ -0,0 +1,84 @@
+﻿using ShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1461467DAWEB.Areas.Admin.Controllers
+{
+    public class ProductTypeController : Controller
+    {
+        // GET: Admin/ProductType
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Admin/ProductType/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/ProductType/Create
+        [HttpPost]
+        public ActionResult Create(LoaiSanPham lsp)
+        {
+            try
+            {
+
+                _1461467DAWEB.Areas.Admin.Models.ProductType.insertProductType(lsp);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/ProductType/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/ProductType/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/ProductType/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/ProductType/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ 1461467DAWEB/Areas/Admin/Models/ProductType.cs
@@ -0,0 +1,50 @@
+﻿using ShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1461467DAWEB.Areas.Admin.Models
+{
+    public class ProductType
+    {
+        public static IEnumerable<LoaiSanPham> ListProductType()
+        {
+            var db = new ShopConnectionDB();
+            return db.Query<LoaiSanPham>("select * from LoaiSanPham");
+        }
+
+        public static void insertProductType(LoaiSanPham lsp)
+        {
+            lsp.Insert();
+        }
+
+        public static LoaiSanPham GetProductType(int id)
+        {
+            var db = new ShopConnectionDB();
+            return db.SingleOrDefault<LoaiSanPham>("select * from LoaiSanPham where MaLoaiSanPham=@0", id);
+        }
+
+        public static void UpdateProductType(LoaiSanPham lsp)
+        {
+            using (var db = new ShopConnectionDB())
+            {
+                db.Update<ProductType>("SET TenLoaiSanPham=@0 WHERE MaLoaiSanPham=@1", lsp._LoaiSanPham, lsp.TenLoaiSanPham);
+            }
+        }
+
+        public static void DeleteProductType(int id)
+        {
+            try
+            {
+                using (var db = new ShopConnectionDB())
+                {
+                    db.Execute("delete from loaisanpham where MaLoaiSanPham = @0", id);
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
