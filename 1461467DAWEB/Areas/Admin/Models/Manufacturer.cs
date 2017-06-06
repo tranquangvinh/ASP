55d8b15440828f880e92cbe6608f12b103cf06e9
HT-PC\PC <vhtrung19031995@gmail.com>
2017-06-06 17:29:12 +0700

    hoan thanh chuc nang phan trang Hang san xuat trong ADMIN

--- 1461467DAWEB/Areas/Admin/Models/Manufacturer.cs
+++ 1461467DAWEB/Areas/Admin/Models/Manufacturer.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using ShopConnection;
+using PetaPoco;
 namespace _1461467DAWEB.Areas.Admin.Models
 {
     public class Manufacturer
     {
+        public static Page<HangSanXuat> ByListManufacturer(int pageNumber, int itemPage)
+        {
+            var db = new ShopConnectionDB();
+            return db.Page<HangSanXuat>(pageNumber, itemPage, " select * from HangSanXuat");
+        }
         public static IEnumerable<HangSanXuat> ListManufacturer()
         {
             var db = new ShopConnectionDB();
             return db.Query<HangSanXuat>("select * from HangSanXuat");
         }
-
         public static void insertManufacturer(HangSanXuat lsp)
         {
             lsp.Insert();
