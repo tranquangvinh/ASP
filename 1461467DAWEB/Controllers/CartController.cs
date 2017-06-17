3904605393ef05c35020c9b88fa7a7596c200c19
huynhquangminh <huynhtony08@gmail.com>
2017-06-17 12:00:54 -0700

    hoan thanh chuc nang them san pham vao gio hang

---
+++ 1461467DAWEB/Controllers/CartController.cs
@@ -0,0 +1,55 @@
+﻿using _1461467DAWEB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1461467DAWEB.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public int CheckIDSP(int id)
+        {
+            List<Item> cart = (List<Item>)Session["Cart"];
+            for(int i = 0; i < cart.Count; i++)
+            {
+                if(cart[i].GetSP().MaSanPham == id)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public ActionResult Create(int id)
+        {
+            if (Session["Cart"] == null)
+            {
+                List<Item> cart = new List<Item>();
+                cart.Add(new Item(SanPhamBus.ListProductsDetails(id), 1));
+                Session["Cart"] = cart;
+            }
+            else
+            {
+                if(CheckIDSP(id) == -1)
+                {
+                    List<Item> cart = (List<Item>)Session["Cart"];
+                    cart.Add(new Item(SanPhamBus.ListProductsDetails(id), 1));
+                    Session["Cart"] = cart;
+                }
+                else
+                {
+                    List<Item> cart = (List<Item>)Session["Cart"];
+                    cart[CheckIDSP(id)].setQuantity(cart[CheckIDSP(id)].GetSL() + 1);
+                    Session["Cart"] = cart;
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ 1461467DAWEB/Models/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShopConnection;
+namespace _1461467DAWEB.Models
+{
+    public class Item
+    {
+        public SanPham sp = new SanPham();
+        public int quantity;
+
+        public void setSP(SanPham sanpham)
+        {
+            sp = sanpham;
+        }
+        public void setQuantity(int sl)
+        {
+            quantity = sl;
+        }
+        public SanPham GetSP()
+        {
+            return sp;
+        }
+        public int GetSL()
+        {
+            return quantity;
+        }
+        public Item(SanPham sanpham, int sl)
+        {
+            sp = sanpham;
+            quantity = sl;
+        }
+    }
+}
